e771e29174ccc3720d0c0bec2e7585c096156674
Febri Kukuh <santosofebrikukuh@gmail.com>
2022-06-24 21:43:17 +0700

    adding change

--- admin/index.php
+++ admin/index.php
@@ -46,69 +46,69 @@ $statement->execute(array('Pending'));
 $total_order_pending = $statement->rowCount();
 
 $statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE payment_status=? AND shipping_status=?");
-$statement->execute(array('Completed','Pending'));
+$statement->execute(array('Completed', 'Pending'));
 $total_order_complete_shipping_pending = $statement->rowCount();
 ?>
 
 <section class="content">
-<div class="row">
-            <div class="col-lg-3 col-xs-6">
+	<div class="row">
+		<div class="col-lg-3 col-xs-6">
 
-              <div class="small-box bg-primary">
-                <div class="inner">
-                  <h3><?php echo $total_product; ?></h3>
+			<div class="small-box bg-primary">
+				<div class="inner">
+					<h3><?php echo $total_product; ?></h3>
 
-                  <p>Products</p>
-                </div>
-                <div class="icon">
-                  <i class="ionicons ion-android-cart"></i>
-                </div>
+					<p>Products</p>
+				</div>
+				<div class="icon">
+					<i class="ionicons ion-android-cart"></i>
+				</div>
 
-              </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-xs-6">
+			</div>
+		</div>
+		<!-- ./col -->
+		<div class="col-lg-3 col-xs-6">
 
-							<div class="small-box bg-maroon">
-								<div class="inner">
-								<h3><?php echo $total_end_category; ?></h3>
+			<div class="small-box bg-maroon">
+				<div class="inner">
+					<h3><?php echo $total_end_category; ?></h3>
 
-								<p>End Categories</p>
-								</div>
-								<div class="icon">
-								<i class="ionicons ion-arrow-down-b"></i>
-								</div>
+					<p>End Categories</p>
+				</div>
+				<div class="icon">
+					<i class="ionicons ion-arrow-down-b"></i>
+				</div>
 
-							</div>
-              <!-- <div class="small-box bg-maroon">
-                <div class="inner">
-                  <h3><?php echo $total_order_pending; ?></h3>
+			</div>
+			<div class="small-box bg-maroon">
+				<div class="inner">
+					<h3><?php echo $total_order_pending; ?></h3>
 
-                  <p>Pending Orders</p>
-                </div>
-                <div class="icon">
-                  <i class="ionicons ion-clipboard"></i>
-                </div>
+					<p>Pending Orders</p>
+				</div>
+				<div class="icon">
+					<i class="ionicons ion-clipboard"></i>
+				</div>
 
-              </div> -->
-            </div>
-            <!-- ./col -->
-            <!-- <div class="col-lg-3 col-xs-6">
+			</div>
+		</div>
+		<!-- ./col -->
+		<div class="col-lg-3 col-xs-6">
 
-              <div class="small-box bg-green">
-                <div class="inner">
-                  <h3><?php echo $total_order_completed; ?></h3>
+			<div class="small-box bg-green">
+				<div class="inner">
+					<h3><?php echo $total_order_completed; ?></h3>
 
-                  <p>Completed Orders</p>
-                </div>
-                <div class="icon">
-                  <i class="ionicons ion-android-checkbox-outline"></i>
-                </div>
+					<p>Completed Orders</p>
+				</div>
+				<div class="icon">
+					<i class="ionicons ion-android-checkbox-outline"></i>
+				</div>
 
-              </div>
-            </div> -->
-            <!-- ./col -->
-            <!-- <div class="col-lg-3 col-xs-6">
+			</div>
+		</div>
+		<!-- ./col -->
+		<!-- <div class="col-lg-3 col-xs-6">
 
               <div class="small-box bg-aqua">
                 <div class="inner">
@@ -122,11 +122,11 @@ $total_order_complete_shipping_pending = $statement->rowCount();
 
               </div>
             </div> -->
-			<!-- ./col -->
+		<!-- ./col -->
 
-			<div class="col-lg-3 col-xs-6">
+		<div class="col-lg-3 col-xs-6">
 
-				<!-- <div class="small-box bg-orange">
+			<!-- <div class="small-box bg-orange">
 					<div class="inner">
 						<h3><?php echo $total_order_complete_shipping_pending; ?></h3>
 
@@ -137,26 +137,26 @@ $total_order_complete_shipping_pending = $statement->rowCount();
 					</div>
 
 				</div> -->
-			  </div>
+		</div>
 
-			  <div class="col-lg-3 col-xs-6">
+		<div class="col-lg-3 col-xs-6">
 
-				<!-- <div class="small-box bg-red">
-				  <div class="inner">
+			<div class="small-box bg-red">
+				<div class="inner">
 					<h3><?php echo $total_customers; ?></h3>
 
 					<p>Active Customers</p>
-				  </div>
-				  <div class="icon">
+				</div>
+				<div class="icon">
 					<i class="ionicons ion-person-stalker"></i>
-				  </div>
+				</div>
 
-				</div> -->
-			  </div>
+			</div>
+		</div>
 
-			  <div class="col-lg-3 col-xs-6">
+		<div class="col-lg-3 col-xs-6">
 
-				<!-- <div class="small-box bg-yellow">
+			<!-- <div class="small-box bg-yellow">
 				  <div class="inner">
 					<h3><?php echo $total_subscriber; ?></h3>
 
@@ -167,11 +167,11 @@ $total_order_complete_shipping_pending = $statement->rowCount();
 				  </div>
 
 				</div> -->
-			  </div>
+		</div>
 
-			  <div class="col-lg-3 col-xs-6">
+		<div class="col-lg-3 col-xs-6">
 
-				<!-- <div class="small-box bg-teal">
+			<!-- <div class="small-box bg-teal">
 				  <div class="inner">
 					<h3><?php echo $available_shipping; ?></h3>
 
@@ -182,54 +182,53 @@ $total_order_complete_shipping_pending = $statement->rowCount();
 				  </div>
 
 				</div> -->
-			  </div>
+		</div>
 
-			  <div class="col-lg-3 col-xs-6">
+		<div class="col-lg-3 col-xs-6">
 
-				<div class="small-box bg-olive">
-				  <div class="inner">
+			<div class="small-box bg-olive">
+				<div class="inner">
 					<h3><?php echo $total_top_category; ?></h3>
 
 					<p>Top Categories</p>
-				  </div>
-				  <div class="icon">
-					<i class="ionicons ion-arrow-up-b"></i>
-				  </div>
-
 				</div>
-			  </div>
+				<div class="icon">
+					<i class="ionicons ion-arrow-up-b"></i>
+				</div>
 
-			  <div class="col-lg-3 col-xs-6">
+			</div>
+		</div>
 
-				<div class="small-box bg-blue">
-				  <div class="inner">
+		<div class="col-lg-3 col-xs-6">
+
+			<div class="small-box bg-blue">
+				<div class="inner">
 					<h3><?php echo $total_mid_category; ?></h3>
 
 					<p>Mid Categories</p>
-				  </div>
-				  <div class="icon">
-					<i class="ionicons ion-android-menu"></i>
-				  </div>
-
 				</div>
-			  </div>
+				<div class="icon">
+					<i class="ionicons ion-android-menu"></i>
+				</div>
 
-			  <div class="col-lg-3 col-xs-6">
+			</div>
+		</div>
 
-				<!-- <div class="small-box bg-maroon">
-				  <div class="inner">
+		<div class="col-lg-3 col-xs-6">
+			<div class="small-box bg-maroon">
+				<div class="inner">
 					<h3><?php echo $total_end_category; ?></h3>
 
 					<p>End Categories</p>
-				  </div>
-				  <div class="icon">
+				</div>
+				<div class="icon">
 					<i class="ionicons ion-arrow-down-b"></i>
-				  </div>
+				</div>
 
-				</div> -->
-			  </div>
+			</div>
+		</div>
 
-		  </div>
+	</div>
 
 </section>
 
